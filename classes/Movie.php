c396cb2042483ca4afd38d75f2ba0a847b09c5ef
Shelbot77 <alessiodeangelis021@gmail.com>
2023-02-03 15:47:40 +0100

    definisco attributi e costruttore

--- classes/Movie.php
+++ classes/Movie.php
@@ -2,4 +2,17 @@
 
 class Movie
 {
+    //attributi
+    public $name;
+    public $duration;
+    public $genre;
+
+
+    //construttore---> Inizializzo una nuova istanza di tipo Movie:
+    public function __construct($_name, $_duration, $_genre)
+    {
+        $this->name = $_name;
+        $this->duration = $_duration;
+        $this->name = $_genre;
+    }
 }
